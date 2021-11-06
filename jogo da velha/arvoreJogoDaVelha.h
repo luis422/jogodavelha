61e3d2020afc11dc136a01571e623c91c6ff85bd
luis422 <luisgufreitas1989@gmail.com>
2021-11-06 17:27:51 -0300

    -Estrutura da árvore que a IA usará para criar as possibilidades foi criada

--- jogo da velha/arvoreJogoDaVelha.h
+++ jogo da velha/arvoreJogoDaVelha.h
@@ -1,120 +1,30 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct no{
 	char tab[3][3];
 	struct no* filhos[9];//númerto máximo de filhos
 };
 
-struct no* raiz = NULL;
-
-void jogadaRandom(char caractere, char tabuleiro[3][3]){
-
-    int linha, coluna;
-    do{
-
-        linha = rand() % 3;
-        coluna = rand() % 3;
-
-    }while(tabuleiro[linha][coluna] != ' ');
-
-    //faz a jogada aleatória
-    tabuleiro[linha][coluna] = caractere;
-
-
+struct noIA{
+	char tab[3][3];
+	struct noIA* filhos[9];//número máximo de filhos
+	int valor;
 };
 
-void IA(char caractere, char tabuleiro[3][3]){
-
-
-}
-
-//pede para o jogador digitar
-void jogar(char caractere, char nome[100], char tabuleiro[3][3]){
-    int  linha, coluna;
-    printf("Digite a posição da jogada(ex: 1 1). Vez de: %s", nome);
-    scanf("%d %d", &linha, &coluna);
-
-    if(tabuleiro[linha][coluna] == ' '){
-        tabuleiro[linha][coluna] = caractere;
-    }else{
-        printf("Essa posição já está ocupada!\n");
-        jogar(caractere, nome, tabuleiro);
-    }
-
-}
-
-/*
- * Executa uma verificação para saber se o jogo acabou e se há um vencedor
- */
-bool alguemGanhou(char tabuleiro[3][3], char nome1[100], char nome2[100]){
-
-    if( (tabuleiro[0][0] == tabuleiro[0][1] && tabuleiro[0][0] == tabuleiro[0][2] && tabuleiro[0][0] == 'X')||
-        (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[0][0] == tabuleiro[2][2] && tabuleiro[0][0] == 'X')||
-        (tabuleiro[0][0] == tabuleiro[1][0] && tabuleiro[0][0] == tabuleiro[2][0] && tabuleiro[0][0] == 'X')||
-        (tabuleiro[0][1] == tabuleiro[1][1] && tabuleiro[0][1] == tabuleiro[2][1] && tabuleiro[0][1] == 'X')||
-        (tabuleiro[0][2] == tabuleiro[1][2] && tabuleiro[0][2] == tabuleiro[2][2] && tabuleiro[0][2] == 'X')||
-        (tabuleiro[1][0] == tabuleiro[1][1] && tabuleiro[1][0] == tabuleiro[1][2] && tabuleiro[1][0] == 'X')||
-        (tabuleiro[2][0] == tabuleiro[2][1] && tabuleiro[2][0] == tabuleiro[2][2] && tabuleiro[2][0] == 'X')||
-        (tabuleiro[2][0] == tabuleiro[1][1] && tabuleiro[2][0] == tabuleiro[0][2] && tabuleiro[2][0] == 'X')){
-        //o jogador 1(X) ganhou
-
-        for(int i=0; i<15; i++){
-            printf("\n");
-        }
-
-        mostrarJogo(tabuleiro);
-        printf("Ganhador: %s", nome1);
-        return true;
-
-    }else
-    if( (tabuleiro[0][0] == tabuleiro[0][1] && tabuleiro[0][0] == tabuleiro[0][2] && tabuleiro[0][0] == 'O')||
-        (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[0][0] == tabuleiro[2][2] && tabuleiro[0][0] == 'O')||
-        (tabuleiro[0][0] == tabuleiro[1][0] && tabuleiro[0][0] == tabuleiro[2][0] && tabuleiro[0][0] == 'O')||
-        (tabuleiro[0][1] == tabuleiro[1][1] && tabuleiro[0][1] == tabuleiro[2][1] && tabuleiro[0][1] == 'O')||
-        (tabuleiro[0][2] == tabuleiro[1][2] && tabuleiro[0][2] == tabuleiro[2][2] && tabuleiro[0][2] == 'O')||
-        (tabuleiro[1][0] == tabuleiro[1][1] && tabuleiro[1][0] == tabuleiro[1][2] && tabuleiro[1][0] == 'O')||
-        (tabuleiro[2][0] == tabuleiro[2][1] && tabuleiro[2][0] == tabuleiro[2][2] && tabuleiro[2][0] == 'O')||
-        (tabuleiro[2][0] == tabuleiro[1][1] && tabuleiro[2][0] == tabuleiro[0][2] && tabuleiro[2][0] == 'O')){
-        //o jogador 2(O) ganhou
-
-        for(int i=0; i<15; i++){
-            printf("\n");
-        }
-
-        mostrarJogo(tabuleiro);
-        printf("Ganhador: %s", nome2);
-        return true;
-
-    }else
-    if( tabuleiro[0][0] != ' ' && tabuleiro[0][1] != ' ' && tabuleiro[0][2] != ' ' &&
-        tabuleiro[1][0] != ' ' && tabuleiro[1][1] != ' ' && tabuleiro[1][2] != ' ' &&
-        tabuleiro[2][0] != ' ' && tabuleiro[2][1] != ' ' && tabuleiro[2][2] != ' ' ){
-        //deu velha
-
-        for(int i=0; i<15; i++){
-            printf("\n");
-        }
-
-        mostrarJogo(tabuleiro);
-        printf("Deu velha\n");
-        return true;
-
-    }
-
-    //o jogo ainda não acabou
-    return false;
-}
+struct no* raiz = NULL;
+struct noIA* raizIA = NULL;
 
 
 //mostra o tabuleiro do jogo
-void mostrarJogo(char tabuleiro[3][3]){
+void mostrarJogo(char t[3][3]){
 
 	printf("\n\n      0   1   2\n\n");
 
 	for(int i=0; i<3; i++){
-		printf("  %d   %c | %c | %c\n", i, tabuleiro[i][0], tabuleiro[i][1], tabuleiro[i][2]);
+		printf("  %d   %c | %c | %c\n", i, t[i][0], t[i][1], t[i][2]);
 
 		//formatação do texto na tela
 		if(i!=2){
@@ -123,3 +33,215 @@ void mostrarJogo(char tabuleiro[3][3]){
 	}
     printf("\n");
 }
+
+//faz uma jogada aleatória
+void jogadaRandom(char caractere, char t[3][3]){
+
+    int linha, coluna;
+    do{
+        srand(time(NULL));
+        linha = rand() % 3;
+        coluna = rand() % 3;
+
+    }while(t[linha][coluna] != ' ');
+
+    //faz a jogada aleatória
+    t[linha][coluna] = caractere;
+
+
+};
+
+bool deuVelha(char t[3][3]){
+
+    if( t[0][0] != ' ' && t[0][1] != ' ' && t[0][2] != ' ' &&
+        t[1][0] != ' ' && t[1][1] != ' ' && t[1][2] != ' ' &&
+        t[2][0] != ' ' && t[2][1] != ' ' && t[2][2] != ' ' ){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+bool marcouPonto(char c, char t[3][3]){
+    if( (t[0][0] == t[0][1] && t[0][0] == t[0][2] && t[0][0] == c)||
+        (t[0][0] == t[1][1] && t[0][0] == t[2][2] && t[0][0] == c)||
+        (t[0][0] == t[1][0] && t[0][0] == t[2][0] && t[0][0] == c)||
+        (t[0][1] == t[1][1] && t[0][1] == t[2][1] && t[0][1] == c)||
+        (t[0][2] == t[1][2] && t[0][2] == t[2][2] && t[0][2] == c)||
+        (t[1][0] == t[1][1] && t[1][0] == t[1][2] && t[1][0] == c)||
+        (t[2][0] == t[2][1] && t[2][0] == t[2][2] && t[2][0] == c)||
+        (t[2][0] == t[1][1] && t[2][0] == t[0][2] && t[2][0] == c)){
+            return true;
+    }else{
+        return false;
+    }
+}
+
+//pede para o jogador digitar
+void jogar(char caractere, char nome[100], char t[3][3]){
+    int  linha, coluna;
+    printf("Digite a posição da jogada(ex: 1 1). Vez de: %s", nome);
+    scanf("%d %d", &linha, &coluna);
+
+    if(t[linha][coluna] == ' '){
+        t[linha][coluna] = caractere;
+    }else{
+        printf("Essa posição já está ocupada!\n");
+        jogar(caractere, nome, t);
+    }
+
+}
+
+//Executa uma verificação para saber se o jogo acabou e se há um vencedor
+bool finalDoJogo(char t[3][3], char nome1[100], char nome2[100]){
+
+    printf("\n");
+    //mostrarJogo(tabuleiro);
+
+    if(marcouPonto('X', t)){
+        //o jogador 1(X)
+        printf("Ganhador: %s", nome1);
+        return true;
+
+    }else if(marcouPonto('O', t)){
+        //o jogador 2(O)
+        printf("Ganhador: %s", nome2);
+        return true;
+
+    }else if(deuVelha(t)){
+        //deu velha
+        printf("Deu velha\n");
+        return true;
+    }
+
+    //o jogo ainda não acabou
+    return false;
+}
+
+
+
+
+void minimax(char meuCaracter, char oponenteCaracter, struct noIA** raizIA){
+
+    bool folha = true;
+
+    //1 - avaliar se é folha
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if((*raizIA)->tab[i][j] == ' '){
+                folha = false;
+                i=j=3;
+            }
+        }
+        if(folha){
+            //calcular a heuristica
+
+            //avaliar se a IA ganhou, perdeu ou deu velha
+            if(marcouPonto(meuCaracter,(*raizIA)->tab)){
+                //se eu ganhei
+                (*raizIA)->valor = 1;
+                return;
+
+            }else if(marcouPonto(oponenteCaracter,(*raizIA)->tab)){
+                //se o oponente ganhou
+                (*raizIA)->valor = -1;
+                return;
+
+            }else if(deuVelha((*raizIA)->tab)){
+                //se deu velha
+                (*raizIA)->valor = 0;
+                return;
+            }
+
+            //fazer chamada recursiva com for, e if para ver se os filhos são nulos ou não
+            for(int i=0; i<9; i++){
+                if((*raizIA)->filhos[i]!=NULL){
+                    minimax(meuCaracter, oponenteCaracter, &(*raizIA)->filhos[i]);
+                }
+            }
+
+        }//if folha
+    }
+
+}
+
+
+
+void criarArvore(char meuCaracter, char oponenteCaracter, struct noIA** raizIA, bool minhaVez){
+
+    if((*raizIA)!=NULL){
+
+        //rodando todas as posições do tabuleiro pai
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+
+                //1 - para cada espaço em branco no tabuleiro do pai...
+                if((*raizIA)->tab[i][j] == ' '){
+
+                    //2 - alocar/criar um novo filho
+                    (*raizIA)->filhos[(2*i)+j+i] = (struct noIA*) malloc(sizeof(struct noIA));
+                    //3 - copiar tabuleiro do pai nos filhos
+                    for(int k=0; k<3; k++){
+                        for(int l=0; l<3; l++){
+                            (*raizIA)->filhos[(2*i)+j+i]->tab[k][l] = (*raizIA)->tab[k][l];
+                        }
+                    }
+
+                    //4 - fazer a jogada de acordo com a rodada
+                    if(minhaVez)
+                        (*raizIA)->filhos[(2*i)+j+i]->tab[i][j] = meuCaracter;
+                    else
+                        (*raizIA)->filhos[(2*i)+j+i]->tab[i][j] = oponenteCaracter;
+
+                    //5 - repetir o processo para cada filho criado
+                    criarArvore(meuCaracter,oponenteCaracter,&(*raizIA)->filhos[2*i+j+i],!minhaVez);
+
+                }else{
+                    //1.2 - senão tiver espaço em branco, o filho será nulo naquela posição
+                    (*raizIA)->filhos[2*i+j+i] = NULL;
+                }
+            }
+        }
+    }
+}
+
+//algoritmo responsável por 'pensar' qual é a melhor jogada a seguir
+void IA(char meuCaracter, char oponenteCaracter, char t[3][3]){
+
+    //alocando árvore usada pela IA
+    raizIA = (struct noIA*) malloc(sizeof(struct noIA));
+    int linha, coluna;
+
+    //criando cópia do tabuleiro principal, para a àrvore de simulação da IA
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            raizIA->tab[i][j] = t[i][j];
+        }
+    }
+    //Passo 1 - criar a árvore de possibilidades
+    criarArvore(meuCaracter, oponenteCaracter, &raizIA, true);
+
+    //Passo 2 - calcular minimax
+    minimax(meuCaracter, oponenteCaracter, &raizIA);
+
+    //Passo 3 - decidir a melhor jogada
+    //...
+
+    //Passo 4 - jogar
+    raiz->tab[linha][coluna];
+
+    //resetar a árvore da IA
+    free(raizIA);
+}
+
+
+
+
+
+
+
+
+
+
+
--- jogo da velha/main.c
+++ jogo da velha/main.c
@@ -7,10 +7,11 @@
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "Portuguese");
 
+    //alocando árvore principal
+    raiz = (struct no*)malloc(sizeof(struct no));
     //variáveis
     int vez = 2, op, contRand = 1;
-    raiz = (struct no*)malloc(sizeof(struct no));
-    char tabuleiro[3][3] = {' ',' ',' ',' ',' ',' ',' ',' ',' '},
+    char tabuleiro[3][3],
          nome1[100],
          nome2[100];
 
@@ -18,70 +19,61 @@ int main(int argc, char *argv[]) {
     //loop para jogar mais rodadas
     do{
 
+        //resetando tabuleiro
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                tabuleiro[i][j] = ' ';
+            }
+        }
+        //resetando variável vez para começar uma nova rodada
+        vez = 2;
+        contRand = 1;
+
+        //limpa a tela a cada nova rodada
+        system("cls || clear");
         //pedindo o nome dos jogadores
         printf("\n  #####################\n  ##  JOGO DA VELHA  ##\n  #####################\n\n");
         printf("Digite o nome do jogador 1(X): ");
-        fgets(nome1,100,stdin);
+        _flushall();
+        fgets(nome1, 100, stdin);
         printf("Digite o nome do jogador 2(O): ");
-        fgets(nome2,100,stdin);
+        fgets(nome2, 100, stdin);
 
-        //loop para escolher
+
+        //loop para jogadas
         do{
+
             mostrarJogo(tabuleiro);
 
             //se a vez for par, jogador 1
             if(vez%2==0){
-
                 jogar('X', nome1, tabuleiro);
 
             //se a vez for ímpar, jogador 2
             }else{
-
+                printf("Vez da IA");
                 //jogada aleatória
                 if(contRand <= 2)
                     jogadaRandom('O', tabuleiro);
-
                 else
-                    IA('O', tabuleiro);
-
-
-                contRand = contRand+1;
+                    IA('O', 'X', tabuleiro);
+                contRand++;
 
             }
 
-
-
-
-
-            //copiando matriz para a raiz
-            //raiz->tab = tabuleiro;
-
+            //copiando tabuleiro para a raiz principal
             for(int i=0; i<3; i++)
                 for(int j=0; j<3; j++)
                     raiz->tab[i][j] = tabuleiro[i][j];
-
-            for(int i=0; i<3; i++){
-                printf("\n");
-                for(int j=0; j<3; j++){
-                    printf("  %c.",raiz->tab[i][j]);
-                }
-            }
-
-
-
-
-
-
-
 
 
             //atualiza a vez dos jogadores
             vez++;
 
             //verificação para saber se alguém ganhou
-        }while(!alguemGanhou(tabuleiro, nome1, nome2));
+        }while(!finalDoJogo(raiz->tab, nome1, nome2));
 
-        printf("\nDeseja jogar novamente?\n[1 - Sim]\n[2 - Não]");
+        printf("\nDeseja jogar novamente?\n[1 - Sim]\n[2 - Não]\n");
         scanf("%d", &op);
 
     }while(op==1);
